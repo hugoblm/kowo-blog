bc36b2d0ab2668077682a9c5b65034badfa74e10
Hugo <hugogblum@gmail.com>
2023-06-16 21:59:27 +0200

    updating favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,12 +9,12 @@ export default class MyDocument extends Document {
       <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
         <Html lang='en'>
           <Head>
-            <link rel='shortcut icon' href='/favicon.ico' />
+            <link rel='shortcut icon' href='/kowo_favicon.ico' />
             <link
               rel='icon'
               type='image/png'
-              sizes='32x32'
-              href='favicon.png'
+              sizes='48x48'
+              href='kowo_favicon_48.png'
             />
 
             <link rel='manifest' href='/manifest.json' />
